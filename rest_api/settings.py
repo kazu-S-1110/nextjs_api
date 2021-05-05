22f5f2ee5ab72671c85886490892cec3d4053cee
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-05-05 19:07:58 +0900

    retry fix

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://nextjs-api210427.herokuapp.com"]
 
 
 # Application definition
